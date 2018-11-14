c9fb8418961aad9d262ec2f32a01e0d6d1c70155
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-11-14 15:41:49 +0100

    Bump version: 0.9.2 → 0.9.3

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'OpenXbox'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.9.2'
+release = '0.9.3'
 
 
 # -- General configuration ---------------------------------------------------
--- xbox/nano/__init__.py
+++ xbox/nano/__init__.py
@@ -3,4 +3,4 @@
 """Top-level package for xbox-smartglass-nano-python."""
 
 __author__ = """OpenXbox"""
-__version__ = '0.9.2'
+__version__ = '0.9.3'
