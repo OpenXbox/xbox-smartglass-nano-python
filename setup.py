08700f070798e04ec34d54c0e2ab98fca59033a9
tuxuser <462620+tuxuser@users.noreply.github.com>
2020-03-26 01:41:34 +0100

    setup.py: tabs vs spaces

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-	"Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8"
     ],
     install_requires=[
         'xbox-smartglass-core>=1.1.0',
